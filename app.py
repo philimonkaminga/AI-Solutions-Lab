0845266306f43aba7c5688dcbefb7d15ae33f3e1
Kaminga Philimon <philimonkaminga@gmail.com>
2025-05-01 16:57:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ if uploaded_file and openai.api_key:
         max_display_width = 1200
         
         if uploaded_file.type.startswith('image'):
-            st.image(uploaded_file, use_column_width=True)
+            st.image(uploaded_file, use_container_width=True)
             image_bytes = uploaded_file.getvalue()
             
         elif uploaded_file.type == "application/pdf":
